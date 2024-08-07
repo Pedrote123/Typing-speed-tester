c3520f6f748b69aa15963d78806f73d7a38d7133
Pedrote123 <[200~pedrocovetta123@gmail.com~>
2024-08-07 18:03:15 -0300

    Logic finish, styling started

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ var RestartButton = document.getElementById('RestartButton');
 var TextToWrite = document.getElementById('TextToWrite');
 var TextWritten = '';
 var WritingSpace = document.getElementById('WritingSpace');
+var Result = document.getElementById('Result');
+var Letter;
+var UpdatedText = TextToWrite.textContent.split('');
+var InputText = '';
+
+WritingSpace.setAttribute('readOnly', true);
 
 Timer.textContent += TimerValue;
 
@@ -14,37 +20,23 @@ Timer.textContent += TimerValue;
 StartButton.addEventListener('click', ()=>{
     if (TimerValue == 30){
         TimerInterval = setInterval(StartTimer, 1000);
+
+        WritingSpace.removeAttribute('readOnly');
+        WritingSpace.addEventListener('input', TextWriting);
     }
 });
 
 RestartButton.addEventListener('click', RestartTimer);
 
-
-var Letter;
-var UpdatedText = TextToWrite.textContent.split('');
-
-//Must fix the matching of the input and the text, cause just detect if the inputed letter is equal, not if is the same text
-
-WritingSpace.addEventListener('input', (e)=>{
-    Letter = UpdatedText[0];
-    Letter = String(Letter);
-
-    console.log(e.data)
-
-    if (e.data == Letter){
-        UpdatedText.splice(0, 1);
-
-        TextWritten += Letter;
-    
-        TextToWrite.innerHTML = `<span id="TextWritten">${TextWritten}</span>` + UpdatedText.join('');
-    }
-})
-
-
-
 function StartTimer(){
     if (TimerValue == 0){
         clearInterval(TimerInterval);
+
+        WritingSpace.setAttribute('readOnly', true);
+        WritingSpace.removeEventListener('input', TextWriting);
+
+        Result.textContent = `${(TextWritten.length / 30).toFixed(2)} keys per second`;
+
     } else{
         TimerValue--;
         Timer.textContent = `Time: ${TimerValue}`;
@@ -57,4 +49,47 @@ function RestartTimer(){
     TimerValue = 30;
 
     Timer.textContent = 'Time: 30';
+
+    WritingSpace.setAttribute('readOnly', true);
+    WritingSpace.removeEventListener('input', TextWriting);
+    WritingSpace.value = '';
+    
+    Result.textContent = '';
+
+    TextToWrite.textContent = TextToWrite.textContent;
+
+    TextWritten = '';
+    InputText = '';
+    UpdatedText = TextToWrite.textContent.split('');
 }
+
+
+function TextWriting(){
+    Letter = UpdatedText[0];
+    Letter = String(Letter);
+
+    var LetterMatching = function(){
+        if (event.data == Letter && TextWritten == InputText){
+            UpdatedText.splice(0, 1);
+    
+            TextWritten += Letter;
+    
+            TextToWrite.innerHTML = `<span id="TextWritten">${TextWritten}</span>` + UpdatedText.join('');
+        }
+    };
+
+    LetterMatching();
+
+    var LettersInputSoFar = function(){
+        if (event.inputType == 'deleteContentBackward'){
+            InputText = InputText.split('');
+            InputText.splice(InputText.length - 1, 1);
+            InputText = InputText.join('')
+        } else{
+            InputText += event.data;
+        }
+    };
+
+    LettersInputSoFar();
+}
+
